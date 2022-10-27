cbe6e66e8c8ab4109912a123b07006a1245eec9c
Nguyen An Vi <nguyenanvi122333gmail.com>
2022-10-27 14:12:45 +0700

    Cai dat chung thuc voi Firebase Authentication

--- lib/ui/screen.dart
+++ lib/ui/screen.dart
@@ -7,3 +7,7 @@ export 'orders/orders_screen.dart';
 export 'orders/orders_manager.dart';
 export 'cart/cart_screen.dart';
 export 'cart/cart_manager.dart';
+
+export 'auth/auth_screen.dart';
+export 'auth/auth_manager.dart';
+export 'splash_screen.dart';
